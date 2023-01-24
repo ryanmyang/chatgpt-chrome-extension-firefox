36520b5d374a28faa4293d70f82e59cad8a7772d
Ryan Yang <ryanmacpro@s-169-232-189-154.resnet.ucla.edu>
2023-01-24 11:43:30 -0800

    nochanges, just test

--- extension/background.js
+++ extension/background.js
@@ -7,8 +7,10 @@ browser.contextMenus.create({
   contexts: ["all"],
 });
 
+
 // Listen for when the user clicks on the context menu item
 browser.contextMenus.onClicked.addListener((info, tab) => {
+  console.log("contextMenu oncliked");
   if (info.menuItemId === "ask-chatgpt") {
     // Send a message to the content script
     browser.tabs.sendMessage(tab.id, { type: "ASK_CHATGPT" });
